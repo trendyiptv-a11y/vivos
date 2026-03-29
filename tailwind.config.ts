3ddbcde256c1ff17decdaf457af2ee2f86cbd2a3
Sergiu B. <trendyiptv@gmail.com>
2026-03-29 14:35:28 +0200

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./lib/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        xl: "1rem",
+        "2xl": "1.25rem",
+        "3xl": "1.5rem",
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config

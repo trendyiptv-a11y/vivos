f38a9fe65d3d187005082610276c232c7febae18
Sergiu B. <trendyiptv@gmail.com>
2026-03-29 15:33:51 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>VIVOS MVP</h1>
+      <p>Deploy test reușit.</p>
+    </main>
+  )
+}

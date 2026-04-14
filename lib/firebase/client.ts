b4d24279c5bd285e5dff7aac61a4e07c30bb87c5
Sergiu B. <trendyiptv@gmail.com>
2026-04-14 17:23:06 +0200

    Update client.ts

--- lib/firebase/client.ts
+++ lib/firebase/client.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps } from "firebase/app"
 import { getMessaging, isSupported } from "firebase/messaging"
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "vivos-3bfba.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "vivos-3bfba.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBSzmSeANo-20hyoYdKjDqNcMiCY6NHK7o",
+  authDomain: "vivos-3bfba.firebaseapp.com",
+  projectId: "vivos-3bfba",
+  storageBucket: "vivos-3bfba.firebasestorage.app",
+  messagingSenderId: "631514497195",
+  appId: "1:631514497195:web:6c0c9b17e24379a75a77ee"
 }
 
 function assertFirebaseConfig() {

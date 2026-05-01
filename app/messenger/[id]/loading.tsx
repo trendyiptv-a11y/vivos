0e6d357c62149aee913c901630f0c346d35e87c2
Sergiu B. <trendyiptv@gmail.com>
2026-05-01 13:35:36 +0200

    Adaugă loading route dedicată pentru conversațiile Messenger

---
+++ app/messenger/[id]/loading.tsx
@@ -0,0 +1,5 @@
+import MessengerConversationLoadingShell from "@/components/messenger/conversation-loading-shell"
+
+export default function LoadingMessengerConversation() {
+  return <MessengerConversationLoadingShell />
+}
